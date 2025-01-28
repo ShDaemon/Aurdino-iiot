9f45da64a52ef94f372550a15edcde187706bd70
Anoop <140630377+daichianoop@users.noreply.github.com>
2025-01-28 16:34:28 +0530

    Create voice-activated-led.ino

---
+++ voice-activated-led.ino
@@ -0,0 +1,109 @@
+/*
+ * Simple example for how to use multiple SinricPro Switch device:
+ * - setup 4 switch devices
+ * - handle request using multiple callbacks
+ * 
+ * If you encounter any issues:
+ * - check the readme.md at https://github.com/sinricpro/esp8266-esp32-sdk/blob/master/README.md
+ * - ensure all dependent libraries are installed
+ *   - see https://github.com/sinricpro/esp8266-esp32-sdk/blob/master/README.md#arduinoide
+ *   - see https://github.com/sinricpro/esp8266-esp32-sdk/blob/master/README.md#dependencies
+ * - open serial monitor and check whats happening
+ * - check full user documentation at https://sinricpro.github.io/esp8266-esp32-sdk
+ * - visit https://github.com/sinricpro/esp8266-esp32-sdk/issues and check for existing issues or open a new one
+ */
+
+
+#ifdef ENABLE_DEBUG
+   #define DEBUG_ESP_PORT Serial
+   #define NODEBUG_WEBSOCKETS
+   #define NDEBUG
+#endif 
+
+#include <Arduino.h>
+#if defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#elif defined(ESP32) || defined(ARDUINO_ARCH_RP2040)
+  #include <WiFi.h>
+#endif
+
+#include "SinricPro.h"
+#include "SinricProSwitch.h"
+
+#define WIFI_SSID         "Merlin"
+#define WIFI_PASS         "244466666"
+#define APP_KEY           "69068261-bf4f-4379-9664-7e31f25d479a"
+#define APP_SECRET        "b72717fc-c62b-45f4-8161-6d6c8ee7ebdc-31dd1bad-c942-4924-99f5-36636c44256a"
+
+#define SWITCH_ID_1       "6798b7a9113ff42c99b93d56"
+#define RELAYPIN_1        12
+
+#define SWITCH_ID_2       "6798b7cbb15d6ea198ec3209"
+#define RELAYPIN_2        13
+
+#define BAUD_RATE         115200                // Change baudrate to your need
+
+bool onPowerState1(const String &deviceId, bool &state) {
+ Serial.printf("Device 1 turned %s", state?"on":"off");
+ digitalWrite(RELAYPIN_1, state ? HIGH:LOW);
+ return true; // request handled properly
+}
+
+bool onPowerState2(const String &deviceId, bool &state) {
+ Serial.printf("Device 2 turned %s", state?"on":"off");
+ digitalWrite(RELAYPIN_2, state ? HIGH:LOW);
+ return true; // request handled properly
+}
+
+// setup function for WiFi connection
+void setupWiFi() {
+  Serial.printf("\r\n[Wifi]: Connecting");
+
+  #if defined(ESP8266)
+    WiFi.setSleepMode(WIFI_NONE_SLEEP); 
+    WiFi.setAutoReconnect(true);
+  #elif defined(ESP32)
+    WiFi.setSleep(false); 
+    WiFi.setAutoReconnect(true);
+  #endif
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.printf(".");
+    delay(250);
+  }
+
+  Serial.printf("connected!\r\n[WiFi]: IP-Address is %s\r\n", WiFi.localIP().toString().c_str());
+}
+
+// setup function for SinricPro
+void setupSinricPro() {
+  // add devices and callbacks to SinricPro
+  pinMode(RELAYPIN_1, OUTPUT);
+  pinMode(RELAYPIN_2, OUTPUT);
+    
+  SinricProSwitch& mySwitch1 = SinricPro[SWITCH_ID_1];
+  mySwitch1.onPowerState(onPowerState1);
+  
+  SinricProSwitch& mySwitch2 = SinricPro[SWITCH_ID_2];
+  mySwitch2.onPowerState(onPowerState2);
+  
+  
+  // setup SinricPro
+  SinricPro.onConnected([](){ Serial.printf("Connected to SinricPro\r\n"); }); 
+  SinricPro.onDisconnected([](){ Serial.printf("Disconnected from SinricPro\r\n"); });
+   
+  SinricPro.begin(APP_KEY, APP_SECRET);
+}
+
+// main setup function
+void setup() {
+  Serial.begin(BAUD_RATE); Serial.printf("\r\n\r\n");
+  setupWiFi();
+  setupSinricPro();
+}
+
+void loop() {
+  SinricPro.handle();
+}
